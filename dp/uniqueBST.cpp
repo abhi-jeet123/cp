c67c853de3102e9f0b5f7f602092aceba8e5770e
Abhishek Trigunait <abhishektrigunait@Admins-MacBook-Pro.local>
2021-08-08 23:06:07 +0530

    https://leetcode.com/problems/unique-binary-search-trees/

---
+++ dp/uniqueBST.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int uniqueBinaryTree(int n){
+    int dp[n+1];
+    memset(dp,0,sizeof(dp));
+    dp[0]=1;
+    dp[1]=1;
+    for(int i=2;i<=n;++i){
+        for(int j=0;j<i;++j){
+            dp[i]+=(dp[j]*dp[i-j-1]);
+        }
+    }return dp[n];
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<uniqueBinaryTree(n)<<endl;
+}
