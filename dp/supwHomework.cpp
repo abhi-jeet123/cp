4610870e17f3176fe6ab2c8e25ce46c18f68ebec
Abhishek Trigunait <abhishektrigunait@192.168.0.104>
2021-08-29 00:15:35 +0530

    https://www.codechef.com/ZCOPRAC/problems/ZCO14002

---
+++ dp/supwHomework.cpp
@@ -0,0 +1,20 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;++i)cin>>arr[i];
+    vector<int> dp(n,0);
+    if(n>=1)dp[0]=arr[0];
+    if(n>=2)dp[1]=arr[1];
+    if(n>=3)dp[2]=arr[2];
+    for(int i=3;i<n;++i){
+        dp[i]=arr[i]+min(dp[i-1],min(dp[i-2],dp[i-3]));
+    }
+    int ans=min(dp[n-1],min(dp[n-2],dp[n-3]));
+    cout<<ans<<endl;
+
+}
