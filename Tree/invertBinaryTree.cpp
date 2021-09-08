b362820b287da10272611a45750b4f6ac1ae1646
Abhishek Trigunait <abhishektrigunait@192.168.0.104>
2021-09-09 00:48:45 +0530

    modified

--- Tree/invertBinaryTree.cpp
+++ Tree/invertBinaryTree.cpp
@@ -22,3 +22,9 @@ public:
         return root;
     }
 };
+
+// (1)  Call Mirror for left-subtree    i.e., Mirror(left-subtree)
+// (2)  Call Mirror for right-subtree  i.e., Mirror(right-subtree)
+// (3)  Swap left and right subtrees.
+//           temp = left-subtree
+//           left-subtree = right-subtree
