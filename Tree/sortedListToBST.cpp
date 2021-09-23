c4298269e48382c8d24edfb3a68ea551c1d0d8cc
Abhishek Trigunait <abhishektrigunait@Admins-MacBook-Pro.local>
2021-09-23 10:51:02 +0530

    https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/submissions/

---
+++ Tree/sortedListToBST.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getMid(ListNode* head){
+    if(head==NULL)return NULL;
+
+    ListNode* slow=head;
+    ListNode* fast=head;
+    ListNode* prev=head;//to break  the link from mid
+    while(fast!=NULL and fast->next!=NULL){
+        prev=slow;
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    if(prev!=NULL){
+        prev->next=NULL;
+    }
+    return slow;
+}
+    
+    TreeNode* sortedListToBST(ListNode* head){
+    if(head==NULL)return NULL;
+
+    ListNode* mid=getMid(head);
+    TreeNode* root=new TreeNode(mid->val);
+    if(head==mid)return root;
+
+    root->left=sortedListToBST(head);
+    root->right=sortedListToBST(mid->next);
+
+    return root;
+  }
+};
