2237ba249124db67e09b630733bcb7677eb19dcf
Abhishek Trigunait <abhishektrigunait@Admins-MacBook-Pro.local>
2021-10-07 01:38:12 +0530

    https://leetcode.com/problems/reverse-linked-list/submissions/

---
+++ DailyInterviewProblem/reverseLinkedList.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   ListNode* helperFunc(ListNode*head,ListNode*prev){
+       if(head==NULL)return prev;
+       ListNode* temp=head->next;
+       head->next=prev;
+       return helperFunc(temp,head);
+       
+   }
+    
+    ListNode* reverseList(ListNode* head) {
+       if(head==NULL)return head;
+        return helperFunc(head,NULL);
+    }
+};
